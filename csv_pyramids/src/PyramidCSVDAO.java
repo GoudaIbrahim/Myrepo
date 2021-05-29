6abad3e9b446ed60ee174f9f3a78b5d0e59f223b
GoudaIbrahim <84772563+GoudaIbrahim@users.noreply.github.com>
2021-05-29 21:07:49 +0200

    Add files via upload

---
+++ csv_pyramids/src/PyramidCSVDAO.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PyramidCSVDAO implements Runnable {
+    String[][] allData = new String[100][15];
+    @Override
+    public void run() {
+        int counter = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("pyramids.csv"));
+            String line = br.readLine();
+            do{
+                line = br.readLine();
+                if (line != null ) {
+                    String[] attributes = line.split(",");
+                    allData[counter] = attributes;
+                    counter ++;
+                }
+            }
+            while(line != null);
+    
+        // for(int i = 0 ; i<counter;i++){
+        //         for (int j = 0; j < allData[i].length; j++) {
+        //             System.out.print(allData[i][j] + " ");
+        //         }
+        //         System.out.println("");
+        //     }
+         } 
+        catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+    public String[][] getAllData() {
+        return allData;
+    }
+   
+    public ArrayList<Pyramid> readPyramidsFromCsv(String filename){
+        ArrayList<Pyramid> pyramids = new ArrayList<Pyramid>();
+        int counter = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String line = br.readLine();
+            do{
+                line = br.readLine();
+                if (line != null ) {
+                    String[] attributes = line.split(",");
+                    allData[counter] = attributes;
+                    counter ++;
+
+                }
+            }
+            while(line != null);
+    
+        for(int i = 0 ; i<counter;i++){
+            pyramids.add(new Pyramid(allData[i][0],allData[i][2],allData[i][4],Double.parseDouble(allData[i][7])));
+            //System.out.println(allData[i][0]+allData[i][2]+allData[i][4]+Double.parseDouble(allData[i][7]));
+         } 
+        }
+        catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return pyramids;
+    }
+}
